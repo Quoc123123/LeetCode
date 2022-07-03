a34483e7802b47042b3fedd6766fb70fa5a23699
Quoc Bui <quocquocbui1999@gmail.com>
2022-07-03 20:53:44 +0700

    88. Merge Sorted Array

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,10 +2,117 @@
 //
 
 #include <iostream>
+#include <vector>
+
+
+/****************************** MERGE SORTED ARRAY **********************************
+************************************************************************************/
+void insertIntoArraySortedNonDecs(std::vector<int>& arr, int element, int sizeCurrent)
+{
+    int lowPosition = 0;
+    int highPosition = sizeCurrent - 1;
+    int midPosition = 0;
+    int lenght = arr.size();
+
+    while(lowPosition < highPosition)
+    {
+        midPosition = (lowPosition + highPosition) / 2;
+    
+        if(arr[midPosition] < element)
+        {
+            lowPosition = midPosition + 1;
+        }
+        else
+        {
+            highPosition = midPosition;
+        }
+    }   
+
+    if(lenght == 1)
+    {
+        arr[lowPosition] = element;
+    }
+    else
+    {
+        if((arr[lowPosition] > element) || (sizeCurrent == 0))
+        {
+            for(int i = lenght - 1; i > lowPosition; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+        }
+        else
+        {
+            for(int i = lenght - 1; i > lowPosition + 1; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            ++lowPosition;
+        }
+        arr[lowPosition] = element;
+    }
+}
+
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+    int totalElementsOfArrayOne = m;
+    int totalElementsOfArrayTwo = n;
+
+    int i = 0;
+    int j = 0;
+    int k = totalElementsOfArrayOne;
+
+    while(((i < totalElementsOfArrayOne) && (j < totalElementsOfArrayTwo)) 
+    || (i < totalElementsOfArrayOne) || (j < totalElementsOfArrayTwo))
+    {
+        if((i < totalElementsOfArrayOne) && (j < totalElementsOfArrayTwo))
+        {
+            if(nums1[i] < nums2[j])
+            {
+               insertIntoArraySortedNonDecs(nums1,  nums2[j], k);
+               ++k;
+               ++j;    
+              
+            }
+            else
+            {
+               ++i; 
+            }
+        }
+        else if(i < totalElementsOfArrayOne)
+        {
+           ++i;
+        }
+        else
+        {
+            insertIntoArraySortedNonDecs(nums1, nums2[j], k);
+            ++j;
+            ++k;
+        }
+    }
+}
+
+
+/****************************** INPUT/OUTPUT **********************************
+************************************************************************************/
+void printArray(std::vector<int>& arr)
+{
+    for(int i = 0; i < arr.size(); i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> nums1 = {1,2,3,0,0,0};
+    std::vector<int> nums2 = {2,5,6};
+    int m = 3;
+    int n = 3;
+    merge(nums1, m, nums2, n);
+    printArray(nums1);
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
